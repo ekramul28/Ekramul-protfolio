24aab1fb00a660d531ef5236efc08161c3d1ea7b
Ekramul <mdekramulhassan168@gmail.com>
2025-01-05 22:02:43 +0600

    update blog section

--- src/components/blog/blog.tsx
+++ src/components/blog/blog.tsx
@@ -5,15 +5,19 @@ import React from "react";
 
 const BlogSection = async () => {
   const blogPosts = await fetchBlog();
+
   return (
-    <section className="w-full  ">
-      <div className="  ">
-        <h2 className="text-3xl font-bold  text-center mb-8">
+    <section className="w-full py-12">
+      <div className="container mx-auto">
+        <h2 className="text-3xl font-bold text-center mb-8">
           Latest Blog Posts
         </h2>
+
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          {blogPosts?.length == 0 ? (
-            <h1 className="text-center">No Blog added</h1>
+          {blogPosts?.length === 0 ? (
+            <h1 className="text-center text-xl text-gray-600">
+              No Blogs Added
+            </h1>
           ) : (
             blogPosts?.map((post: any) => (
               <div
@@ -23,18 +27,22 @@ const BlogSection = async () => {
                 <Image
                   height={200}
                   width={200}
-                  src={post.image}
+                  src={post.imageUrl}
                   alt={post.title}
                   className="w-full h-56 object-cover rounded-md mb-4"
                 />
                 <h3 className="text-2xl font-semibold text-gray-800 mb-4">
                   {post.title}
                 </h3>
-                <p className="text-gray-600 mb-4">
-                  {post.description.length > 20
-                    ? `${post.description.slice(0, 150)}...`
-                    : post.description}
-                </p>{" "}
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html:
+                      post?.content?.length > 150
+                        ? `${post?.content?.slice(0, 150)}...`
+                        : post.content,
+                  }}
+                />
+
                 <a
                   href={post.link}
                   className="inline-block text-blue-500 font-semibold hover:text-blue-700"
